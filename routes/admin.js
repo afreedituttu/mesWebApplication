3fb34ad031780b690524722b55d028de115e23f2
afreedi <afreedisulfiker@gmai.com>
2022-03-10 21:27:55 +0530

    mostly working correctly

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const admin = require('../db/adminModels')
 var { verifyLogin } = require('../middlewares/admin')
-const mongoose = require('mongoose')
+
 var student = require('../db/models')
 router.get('/',verifyLogin,function(req, res, next) {
     res.render('adminD');
@@ -12,8 +13,20 @@ router.get('/login',function(req,res){
 router.post('/login',async(req,res)=>{
   console.log('here it is from admin login');
   console.log(req.body);
-  const admin = student.find({admin:true})
-  console.log(admin);
-  res.send('success')
+
+  const adminsave = await admin.findOne({admin:true})
+  console.log(adminsave);
+  const username=req.body.user
+  const password = req.body.password
+  console.log('username '+username);
+  console.log('password '+password);
+  console.log(adminsave.username);
+  console.log(adminsave.password);
+  if(username===adminsave.username && password===adminsave.password){
+    res.send('success')
+  }else{
+    res.send('fail')
+  }
+
 })
 module.exports = router

8a0388d945ad55baf5d8eef787beb024e7aeff3b
afreedi <53534693+afreedituttu@users.noreply.github.com>
2022-03-14 16:26:29 +0530

    Update admin.js
    Standardized

--- routes/admin.js
+++ routes/admin.js
@@ -11,17 +11,13 @@ router.get('/login',function(req,res){
   res.render('adminLogin')
 })
 router.post('/login',async(req,res)=>{
-  console.log('here it is from admin login');
-  console.log(req.body);
+  
 
   const adminsave = await admin.findOne({admin:true})
-  console.log(adminsave);
+  
   const username=req.body.user
   const password = req.body.password
-  console.log('username '+username);
-  console.log('password '+password);
-  console.log(adminsave.username);
-  console.log(adminsave.password);
+  
   if(username===adminsave.username && password===adminsave.password){
     res.send('success')
   }else{

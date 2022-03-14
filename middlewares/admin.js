5bd9e6202c46cf29552ec09ca00a084c9419aa4b
afreedi <53534693+afreedituttu@users.noreply.github.com>
2022-03-14 16:23:12 +0530

    Update admin.js

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,11 +1,9 @@
 middleWares={verifyLogin:function(req,res,next){
-    console.log('im in verifylogin2')
-    console.log(req.session.logInId);
     if(req.session.logInId){
-        console.log('loged in user');
+        
         next()
     }else{
-        console.log('im in verifylogin');
+        
         res.redirect('http://localhost:3000/code123admin/login')
         next()
     }
